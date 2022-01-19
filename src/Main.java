1e40bdb382f123ced594360e31714491cb98434d
mjn81 <mjjnn81@gmail.com>
2022-01-19 13:57:52 +0330

    Queue implemented without sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Node[] sa = new Node[5];
+        sa[0] = new Node("a" , 6);
+        sa[1] = new Node("b" , 4);
+        sa[2] = new Node("c" , 4);
+        sa[3] = new Node("d" , 2);
+        sa[4] = new Node("e" , 1);
+        Tree[] ta = new Tree[5];
+        for (int i = 0; i < 5; i++) {
+            ta[i] = new Tree(sa[i]);
+        }
+        Queue q = new Queue();
+        for (int i = 0; i < 5; i++) {
+            q.enqueue(ta[i]);
+        }
+        for (int i = 0; i < 5; i++) {
+            Tree smp = q.dequeue();
+            System.out.println(smp);
+        }
 
     }
 }
--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,46 @@
-public class Queue {
+class Queue {
+    private static int front, rear, size;
+    private static Tree dataList[];
+    public Queue(){
+        this(50);
+    }
+    public Queue(int size)
+    {
+        front = rear = 0;
+        this.size = size;
+        dataList = new Tree[size];
+    }
+
+    public void enqueue(Tree data) throws Exception {
+        if(isFull()){
+            throw new Exception("queue is full");
+        }
+        else {
+            dataList[rear] = data;
+            rear++;
+        }
+    }
+
+    public boolean isEmpty(){
+        return front == rear;
+    }
+    public boolean isFull(){
+        return size == rear;
+    }
+
+    public Tree dequeue() {
+        if (isEmpty()) {
+            return null;
+        }
+        Tree data  = dataList[front];
+        for (int i = front; i < rear - 1; i++) {
+            dataList[i] = dataList[i + 1];
+        }
+        if (rear < size)
+            dataList[rear] = null;
+        rear--;
+        return data;
+    }
+
 
 }
