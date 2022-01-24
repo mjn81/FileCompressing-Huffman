4bffb053b1d8b6660f7ef247fd2f70fda03fdc7a
mjn81 <mjjnn81@gmail.com>
2022-01-24 14:14:11 +0330

    Main function added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        Scanner scan  = new Scanner(System.in);
+        int decider = scan.nextInt();
+        scan.nextLine();
+        String txt , res="";
+        switch (decider) {
+            case 1 -> {
+                txt = scan.nextLine();
+                Compressor compress = new Compressor(txt);
+                res = compress.compress();
+            }
+            case 2 -> {
+                int numOfCodes = scan.nextInt();
+                scan.nextLine();
+                String[] inp;
+                HashMap<Character, Integer> codes = new HashMap<>();
+                for (int i = 0; i < numOfCodes; i++) {
+                    inp = scan.nextLine().split(" ");
+                    codes.put(inp[0].charAt(0), Integer.parseInt(inp[1]));
+                }
+                txt = scan.nextLine();
+                Decompressor decompressor = new Decompressor(txt, codes);
+                res = decompressor.decompress();
+            }
+        }
+        System.out.println(res);
 
     }
 }
